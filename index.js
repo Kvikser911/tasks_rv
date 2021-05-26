632c9fd353c013573bc3fc6aea1e5b50d528f15d
Kvikser911 <kvikser911@gmail.com>
2021-05-26 16:29:11 +0300

    implementation 1

---
+++ index.js
@@ -0,0 +1,50 @@
+/*
+let arr = [10, 20, 30, 9, 2, 1, 9, 8];
+function getMin(arr){
+    let result = arr[0];
+   for(let i = 0; i < arr.length; i++ ) {
+          if(result > arr[i]){
+                result = arr[i];
+             } 
+    }
+    return result;
+ 
+}
+console.log(getMin(arr));
+
+*/
+/*let arr = [10, 20, 30, 9, 2, 1, 9, 8];
+function getEven(arr){
+    let result = new Array();
+    for(let i = 0; i < arr.length; i++){
+            if( (arr[i] % 2)==0) {
+                result.push(arr[i]);
+            }
+    }
+    
+    for(let y of result){
+        console.log(y);
+    }
+
+}
+
+getEven(arr);*/
+ 
+var objects= [ 
+    {name:'Jonny Walker', birthDate:'1995-12-17'},
+    {name:'Andrew', birthDate:'2001-10-29'},
+    {name:'Viktor', birthDate:'1998-11-09'},
+    {name :'Andrew', birthDate:'2011-05-09'}
+];
+  
+var results = [];
+
+var toSearch = "Andrew";
+
+for(var i=0; i<objects.length; i++) {
+    for(key in objects[i]) {
+        if(objects[i][key].indexOf(toSearch)!=-1) {
+            console.log(objects[i]);
+        }
+    }
+}
